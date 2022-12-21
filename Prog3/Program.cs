745f30c5c3590892d6003c82e4a01ae23be1b3ec
Dmitry Bober <bober113d@gmail.com>
2022-12-21 18:33:45 +0500

    prog3

---
+++ Prog3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1) 
+{
+    Console.Write("Нечётное  ");
+}
+
+else 
+{
+    Console.Write("Чётное  ");
+}
+
+ 
+
+
